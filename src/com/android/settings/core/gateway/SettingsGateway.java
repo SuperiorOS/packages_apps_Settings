SuperiorOS/android_packages_apps_Settings

Settings: Add SuperiorLab to dashboard
Change-Id: Ie18a29e62a651b4b5c74510493dc291135f5361c
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -195,6 +195,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.superior.lab.SuperiorLab;
+
 public class SettingsGateway {
 
     /**
@@ -377,7 +379,8 @@ public class SettingsGateway {
             LongBackgroundTasksDetails.class.getName(),
             RegionalPreferencesEntriesFragment.class.getName(),
             BatteryInfoFragment.class.getName(),
-            UserAspectRatioDetails.class.getName()
+            UserAspectRatioDetails.class.getName(),
+            SuperiorLab.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -424,5 +427,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.SuperiorLabActivity.class.getName(),
     };
 }

SuperiorOS/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -207,6 +207,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.superior.lab.SuperiorLab;
+
 public class SettingsGateway {
 
     /**
@@ -401,7 +403,8 @@ public class SettingsGateway {
             AccessibilityHearingAidsFragment.class.getName(),
             HearingDevicePairingFragment.class.getName(),
             ZenModesListFragment.class.getName(),
-            ZenModeFragment.class.getName()
+            ZenModeFragment.class.getName(),
+            SuperiorLab.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -448,5 +451,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.SuperiorLabActivity.class.getName(),
     };
 }