cd80a9d054c6863867228905a497bfa960966142
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2026-01-21 12:01:51 +0100

    Settings: Remove LineageOS entries from Settings app

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
@@ -70,13 +70,10 @@ open class FirmwareVersionScreen : PreferenceScreenMixin, PreferenceSummaryProvi
     override fun getPreferenceHierarchy(context: Context, coroutineScope: CoroutineScope) =
         preferenceHierarchy(context) {
             +FirmwareVersionDetailPreference()
-            +LineageVersionDetailPreference()
             +SecurityPatchLevelPreference()
-            +LineageVendorSecurityPatchLevelPreference()
             +MainlineModuleVersionPreference()
             +BasebandVersionPreference()
             +KernelVersionPreference()
-            +LineageBuildDatePreference()
             +SimpleBuildNumberPreference()
         }
 
